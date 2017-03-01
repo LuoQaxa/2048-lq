d0245f5a996901c945ca7937a002cb6369328c17
罗倩 <szuluoqian@163.com>
2017-03-01 17:49:48 +0800

    modified

---
+++ support2048.js
@@ -0,0 +1,111 @@
+
+function getPosTop( i , j ){
+    return 20 + i*120;
+}
+
+function getPosLeft( i , j ){
+    return 20 + j*120;
+}
+
+function getNumberBackgroundColor( number ){
+    switch( number ){
+        case 2:return "#eee4da";break;
+        case 4:return "#ede0c8";break;
+        case 8:return "#f2b179";break;
+        case 16:return "#f59563";break;
+        case 32:return "#f67c5f";break;
+        case 64:return "#f65e3b";break;
+        case 128:return "#edcf72";break;
+        case 256:return "#edcc61";break;
+        case 512:return "#9c0";break;
+        case 1024:return "#33b5e5";break;
+        case 2048:return "#09c";break;
+        case 4096:return "#a6c";break;
+        case 8192:return "#93c";break;
+    }
+
+    return "black";
+}
+
+function getNumberColor( number ){
+    if( number <= 4 )
+        return "#776e65";
+
+    return "white";
+}
+
+function nospace( board ){
+
+    for( var i = 0 ; i < 4 ; i ++ )
+        for( var j = 0 ; j < 4 ; j ++ )
+            if( board[i][j] == 0 )
+                return false;
+
+    return true;
+}
+
+//在这个函数中要遍历234列的所有格子，第一列不用遍历因为第一列肯定是不能向左移动的。
+//如果当前格子保存的数字不为零就要判断当前格子的左边的格子就是j-1列的是否为0 
+//或者是否等于当前格子保存的数字，如果等于或者为0说明可以往左移动。
+function canMoveLeft( board ){
+
+    for( var i = 0 ; i < 4 ; i ++ )
+        for( var j = 1; j < 4 ; j ++ )
+            if( board[i][j] != 0 )
+                if( board[i][j-1] == 0 || board[i][j-1] == board[i][j] )
+                    return true;
+
+    return false;
+}
+
+function canMoveRight( board ){
+
+    for( var i = 0 ; i < 4 ; i ++ )
+        for( var j = 2; j >= 0 ; j -- )
+            if( board[i][j] != 0 )
+                if( board[i][j+1] == 0 || board[i][j+1] == board[i][j] )
+                    return true;
+
+    return false;
+}
+
+function canMoveUp( board ){
+
+    for( var j = 0 ; j < 4 ; j ++ )
+        for( var i = 1 ; i < 4 ; i ++ )
+            if( board[i][j] != 0 )
+                if( board[i-1][j] == 0 || board[i-1][j] == board[i][j] )
+                    return true;
+
+    return false;
+}
+
+function canMoveDown( board ){
+
+    for( var j = 0 ; j < 4 ; j ++ )
+        for( var i = 2 ; i >= 0 ; i -- )
+            if( board[i][j] != 0 )
+                if( board[i+1][j] == 0 || board[i+1][j] == board[i][j] )
+                    return true;
+
+    return false;
+}
+
+//这个函数传入行、第一列和第二列 还有面板四个参数
+//遍历第一列和第二列 
+//比如说是判断1,3列之间是否有障碍，就是判断第二列是否有格子的数字不等于0；
+//所有事col1+1 开始遍历 如果有不等于0的列就 返回false表示有障碍物
+function noBlockHorizontal( row , col1 , col2 , board ){
+    for( var i = col1 + 1 ; i < col2 ; i ++ )
+        if( board[row][i] != 0 )
+            return false;
+    return true;
+}
+
+function noBlockVertical( col , row1 , row2 , board ){
+    for( var i = row1 + 1 ; i < row2 ; i ++ )
+        if( board[i][col] != 0 )
+            return false;
+    return true;
+}
+
